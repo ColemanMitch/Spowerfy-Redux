ca95105f3471c201a5eb4ab0c5a6695eb9be0ef4
Cole Mitchell <wcm519@gmail.com>
2021-03-10 12:02:18 -0600

    Cleaning up conditional rendering for JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 
