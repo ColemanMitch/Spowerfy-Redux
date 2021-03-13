ecc418681e668a9bd163bb49363ce2fd8a65a2df
David Williams <davo49fb@gmail.com>
2021-03-13 17:22:11 -0600

    redo spacing

--- src/App.tsx
+++ src/App.tsx
@@ -131,65 +131,67 @@ class App extends Component<{}, AppState> {
         </header>
         <h2>Currently Playing: </h2>
         <Timer getCurrentlyPlaying={this.getCurrentlyPlaying} skipToNextSong={this.skipToNextSong}></Timer>
-            {this.state.activeSong && this.state.activeSong.item ?
+          {this.state.activeSong && this.state.activeSong.item ?
             <div>
             <img src={this.state.activeSong.item.album.images[0].url} alt='album art of the current track'></img>
             <h3 style={{fontWeight: 'bold'}}>{this.state.activeSong.item.name}</h3>
             <h4 className="artist-name">{this.state.activeSong.item.album.artists[0].name}</h4> 
             </div>
-            :
+          :
             <p>Loading playback..</p>
-            }
+          }
         </div>
         :<div>
-            {this.state.user ?
-            <div>
-                <header className="fixed-header">
-                <h1 className="app-title">Spowerfy 🍺</h1>  
-                <button  className="start-button" style={{float:"right"}} onClick={this.startPlayback}>Click to start your power hour</button>
-                </header>
-                <div className="app-body">
-                <h2>Hello {this.state.user.name},</h2>
-                <br></br>
-                <h3>Your available devices to play from are: </h3>
-                {this.state.devices ?
-                <ul>
-                    <form id="device-select">
-                {this.state.devices.map((device: any) => (
-                    <li className="device"><input type="radio" value={device.id} name="device" onClick={this.handleDevice}/>{device.name}</li>
-                ))}</form>
-                </ul>
-                :
-                <p>Loading devices...</p>
-                }
-                {this.state.playlists && this.state.playlists.length > 0 ?
-                <div className="playlist-container">
-                    <ul>
-                    <form id="playlist-select">
-                    {this.state.playlists.map((pl) => (
-                        <div className="playlist-div">
-                        <img className="playlist-images" src={pl.images[0]?.url} alt="Playlist art"></img>
-                        <li className="playlist-name"><input type="radio" value={pl.uri} name="playlists" onClick={this.handlePlaylist}/>{pl.name}</li>
-                        </div>
-                    ))}
-                    </form>
-                    </ul>
-                </div>
-                : <p>Loading playlists...</p>
-                }
-            <hr></hr>
-                </div>
-            </div>
-            : 
-            <div className="login-app-body">
-           <header className="nonfixed-header">
-            <h1 className="app-title-nonfixed">Spowerfy 🍺</h1>
+          {this.state.user ?
+          <div>
+            <header className="fixed-header">
+              <h1 className="app-title">Spowerfy 🍺</h1>  
+              <button  className="start-button" style={{float:"right"}} onClick={this.startPlayback}>Click to start your power hour</button>
             </header>
-            <button id="sign-in-button" className="center" onClick={() => {
-                window.location.assign('https://spowerfy-backend.herokuapp.com/login') }
+            <div className="app-body">
+            <h2>Hello {this.state.user.name},</h2>
+            <br></br>
+            <h3>Your available devices to play from are: </h3>
+            {this.state.devices ?
+            <ul>
+              <form id="device-select">
+                {this.state.devices.map((device: any) => (
+                  <li className="device" key={device.id}><input type="radio" value={device.id} name="device" onClick={this.handleDevice}/>{device.name}</li>
+                ))}
+              </form>
+            </ul>
+            :
+            <p>Loading devices...</p>
             }
-            style={{'fontSize': '20px'}}>Sign in with Spotify</button>
-            </div>
+            {this.state.playlists && this.state.playlists.length > 0 ?
+              <div className="playlist-container">
+                <ul>
+                <form id="playlist-select">
+                {this.state.playlists.map((pl) => (
+                  <div className="playlist-div">
+                    <img className="playlist-images" src={pl.images[0]?.url} alt="Playlist art"></img>
+                    <li className="playlist-name"><input type="radio" value={pl.uri} name="playlists" onClick={this.handlePlaylist}/>{pl.name}</li>
+                    </div>
+                ))}
+                </form>
+                </ul>
+              </div>
+            : 
+              <p>Loading playlists...</p>
+            }
+          <hr></hr>
+              </div>
+          </div>
+          : 
+          <div className="login-app-body">
+          <header className="nonfixed-header">
+          <h1 className="app-title-nonfixed">Spowerfy 🍺</h1>
+          </header>
+          <button id="sign-in-button" className="center" onClick={() => {
+              window.location.assign('https://spowerfy-backend.herokuapp.com/login') }
+          }
+          style={{'fontSize': '20px'}}>Sign in with Spotify</button>
+          </div>
         }
         </div>
         }
