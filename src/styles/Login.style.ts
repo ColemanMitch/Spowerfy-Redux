57d51a8e48017a54d35f85f69d3d1877a4d15fe5
Cole Mitchell <w.coleman.mitchell@gmail.com>
2024-02-08 15:25:32 -0600

    better mobile breakpoints

--- src/styles/Login.style.ts
+++ src/styles/Login.style.ts
@@ -8,7 +8,7 @@ export const SignInButton = styled.button`
   color: white;
   border: #c9c9c9 solid 1px;
   border-radius: ${MEDIUM};
-  margin-top: 100px;
+  margin-top: 80px; /* Adjust the margin-top value as needed */
   height: 40px !important;
   width: 250px;
   align-items: center;
@@ -25,24 +25,34 @@ export const LoginAppBody = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 100%;
   background: linear-gradient(-45deg, #e4b4a4, #e9bbcd, #77c0b0, #7db6aa);
   background-size: 400% 400%;
   -webkit-animation: gradient 15s ease infinite;
   animation: gradient 5s ease infinite;
+  
 `;
 
 export const InstructionsAndButton = styled.div`
   justify-content: center;
   text-align: center;
   flex-direction: column;
+  @media (max-width: 500px) {
+    width: 70%;
+  }
   width: 30%;
 `;
 
 export const Title = styled.h2`
+  @media (max-width: 500px) {
+    font-size: 2.5rem;
+  }
   font-size: 5rem;
 `;
 
 export const Subtitle = styled.h3`
+  @media (max-width: 500px) {
+    font-size: .9rem;
+  }
   font-size: 1.5rem;
 `;
